b0a97fc7024c866a681a37fd9a9c30ac6170c04c
Robert Frazier <robert.v.frazier@gmail.com>
2017-12-15 17:32:53 -0500

    Wrote another test, got it to work.

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -1,9 +1,34 @@
 import React from 'React'
 import Feedback from './feedback'
 import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
 
 describe('<Feedback />', ()=>{
 	it('Smoke test.', ()=>{
 		shallow(<Feedback />)
 	})
+
+	it('should generate a key that is equal to the guess count', ()=>{
+		const myGuessCount = 1;
+		const wrapper=mount(<Feedback guessCount={myGuessCount} />);
+		expect(wrapper.contains(<span className="visuallyhidden">Guess again!</span>)).toEqual(true);
+	})
+
+	it('should display feedback correctly.', ()=>{
+		const myFeedbackTest = 'Here is some feedback.';
+		const myGuessCount = 1;
+		const wrapper=mount(<Feedback guessCount={myGuessCount} feedback={myFeedbackTest} />);
+		expect(wrapper.contains({myFeedbackTest} {guessAgain}).toEqual(true);
+	})
 })
+
+
+		// <h2
+		// 	key={myGuessCount}
+		// 	id="feedback"
+		// 	role="status"
+		// 	aria-live="assertive"
+		// 	aria-atomic="true"
+		// >
+		// 	{myFeedbackTest} <span className="visuallyhidden">Guess again!</span>;
+		// </h2>)).toEqual(true);
